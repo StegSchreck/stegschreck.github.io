13ffaa609f37c10fcdce6c804dc56428f9cf1637
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2019-07-20 19:09:57 +0200

    Updates

---
+++ precache-manifest.b18519fcc515cbd0e0c80d34d8af6fb9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04070b92e6197c8c9538eeb6ae297bd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f85ae6ffda9c0c479e7",
+    "url": "/static/css/main.427a1069.chunk.css"
+  },
+  {
+    "revision": "21e0455ab157e3c726f6",
+    "url": "/static/js/2.4282bdd0.chunk.js"
+  },
+  {
+    "revision": "6f85ae6ffda9c0c479e7",
+    "url": "/static/js/main.ce982eaf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5333bd748ef61c988a9fb7cd45daa88c",
+    "url": "/static/media/RrQUbo9-9DV7b06QHgSWsahHT4I.5333bd74.woff2"
+  },
+  {
+    "revision": "f243535e1b7a7d8bebcb621acdc70ec2",
+    "url": "/static/media/snfys0G548t04270a_ljTLUVrv-LZxec.f243535e.woff2"
+  },
+  {
+    "revision": "85960347eee6ca643b2cc310bbbdf7d0",
+    "url": "/static/media/snfys0G548t04270a_ljTLUVrv-LaBecc5Y.85960347.woff2"
+  },
+  {
+    "revision": "736100fdcfd4dfc85301b5a090fd452e",
+    "url": "/static/media/snfys0G548t04270a_ljTLUVrv-LaRecc5Y.736100fd.woff2"
+  }
+]);
